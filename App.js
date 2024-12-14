cf2c4f21b976c83244b974bde3944aeee242f4bb
jpalmat <jimmy.palma1990@gmail.com>
2024-12-13 19:33:58 -0600

    add changes

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test</Text>
+      {/* <Image
+                style={styles.image, {height: height * 0.2}}
+                source={require("../assets/images/onStar.jpeg")}
+                resizeMode="contain"
+            /> */}
       <StatusBar style="auto" />
     </View>
   );
